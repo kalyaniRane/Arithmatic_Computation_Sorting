d5645f26b65decbe3df3d10dfd1deac2281951c4
Kalyani <kalyanirane19@gmail.com>
2020-01-22 18:58:04 +0530

    ADDED : Arithmatic Operation1

--- arithmatic_operations.sh
+++ arithmatic_operations.sh
@@ -5,3 +5,8 @@ echo "Welcome To Arithmatic Operations And Sorting"
 read -p "Enter the three numbers : " a b c
 
 echo -e "a=$a \n b=$b \n c=$c"
+
+#Performing the Arithmetic operation
+operation1=$((a + b * c))
+
+echo "((a + b * c)) = $operation1"
