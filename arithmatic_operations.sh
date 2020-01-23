f7714dea4a832dc8c5fbffc9a7db83cdd1bb2a8a
Kalyani <kalyanirane19@gmail.com>
2020-01-23 10:06:55 +0530

    ADDED : Arithmatic Operation3

--- arithmatic_operations.sh
+++ arithmatic_operations.sh
@@ -9,6 +9,9 @@ echo -e "a=$a \n b=$b \n c=$c"
 #Performing the Arithmetic operation
 operation1=$((a + b * c))
 operation2=$((a * b + c))
+operation3=$(echo "scale=1; $c + $a / $b" | bc)
+
 
 echo "((a + b * c)) = $operation1"
 echo "((a * b + c)) = $operation2"
+echo "((c + a / b)) = $operation3"
