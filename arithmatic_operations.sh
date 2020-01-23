76c528e2e4485cd1bc9c592bb16281dc48229b69
Kalyani <kalyanirane19@gmail.com>
2020-01-23 11:50:13 +0530

    ADDED : Store value Dict to array

--- arithmatic_operations.sh
+++ arithmatic_operations.sh
@@ -9,14 +9,30 @@ echo -e "a=$a \n b=$b \n c=$c"
 #declareing a dictionary
 declare -A dict
 
-#Performing the Arithmetic operation
+#Performing the Arithmetic operation and store in into the dictionary
 dict[operation1]=$((a + b * c))
 dict[operation2]=$((a * b + c))
 dict[operation3]=$(echo "scale=1; $c + $a / $b" | bc)
 dict[operation4]=$(echo "$a % $b + $c" | bc)
 
-for((i=1;i<5;i++))
-do
+#Print Dictionary
+
+function print()
+{
+	for((i=1;i<5;i++))
+	do
 		echo "dict[operation$i]=${dict[operation$i]}"
+	done
+}
+
+#function calling
+print
+
+#Store elements of Dictionary into Array
+for((i=0;i<4;i++))
+do
+		arr[i]=${dict[operation$((i+1))]}
 done
 
+#print Array
+echo "Array elements are: "${arr[@]}
